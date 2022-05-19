b6d357100796f5ddf4b95305b306183b7ab9a5c3
Romain Ricord <romain.ricord.pro@gmail.com>
2022-05-19 14:58:11 +0200

    Add FS support

--- index.js
+++ index.js
@@ -22,13 +22,34 @@ app.post('/card',cors(cors_opt),(req,res) => {
 
     const {date,title,distance,duration,image} = req.headers
 
-    cards.push({
+    /*cards.push({
         date:date,
         title:title,
         distance:distance,
         duration:duration,
         image:image
-    })
+    })*/
+
+    const card_ = {
+        date:date,
+        title:title,
+        distance:distance,
+        duration:duration,
+        image:image
+    }
+    
+    fs.readFile('data.json', (err, data) => { // get the data from the file
+        if(data != ''){
+            cards = JSON.parse(data);
+        }
+        cards.push(card_);
+        fs.writeFile('data.json', JSON.stringify(products), (err) => {
+            console.log(err);
+        });            
+    });
+
+    const data = JSON.stringify(card_);
+    fs.writeFileSync('data.json', data);
 
     console.log(cards)
 
